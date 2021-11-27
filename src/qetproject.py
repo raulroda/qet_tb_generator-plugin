81e17438af328ad340be5a4e546e3ef4878568f5
raul <raulroda8@gmail.com>
2021-11-27 10:14:18 +0100

    v1.3.0

--- src/qetproject.py
+++ src/qetproject.py
@@ -2,7 +2,7 @@
 # encoding: utf-8
 
 #---------|---------|---------|---------|---------|---------|---------|---------|
-# Copyright (C) 2018 Raul Roda <raulroda@yahoo.com>
+# Copyright (C) 2018 Raul Roda <raulroda8@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
